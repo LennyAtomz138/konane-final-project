8c7e1ce503d7af6fcdb7d9abc368d80f2e42f1ef
Alex <alex.o'neill@uconn.edu>
2019-12-05 13:18:18 -0500

    Update server_conn.py
    possibly playable script?

--- server_conn.py
+++ server_conn.py
@@ -18,14 +18,21 @@ tn.read_until(b"?Opponent:")
 choice = str(input('Opponent:'))
 tn.write(choice.encode('ascii') + b"\r\n")
 
+flag = False
+
 while True:
     res = tn.read_some()
+    #print(res)
     print(str(res, "utf-8"))
-    #print(str(res, "utf-8"))
     if "Player:1" in str(res, "utf-8"):
         tn.write(input("Remove: ").encode('ascii') + b"\r\n")
+        flag = True
     if "Player:2" in str(res, "utf-8"):
         tn.write(input("Remove: ").encode('ascii') + b"\r\n")
+        flag = True
+
+    if flag && "Move" in str(res, "utf-8"):
+        tn.write(input("Move: ").encode('ascii') + b"\r\n")
 
     if "Error:" in str(res, "utf-8"):
         break
