78f88d5d34f82d0f1985b741bcd198af642d4949
LennyAtomz138 <lenny.atomz@gmail.com>
2019-12-05 15:44:41 -0500

    Added spacing.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from game.player import HumanPlayer
+from game.player import HumanPlayer, AIPlayer, NetworkPlayer
 from game.game import Game
 import sys
 import telnetlib
@@ -7,20 +7,21 @@ if __name__ == '__main__':
 
     print("\nSelect type of game for the AI to play:\n\t1)Human Player\n\t2)Server Player\n\t3)Itself")
     choice = input("--> ")
-    if (choice == "1"):
+
+    if choice == "1":
         print("\nWho goes first?\n\t1)You\n\t2)AI")
         first = input("--> ")
         p0 = HumanPlayer('Player 0')
         p1 = AIPlayer()
-        if (first == "1"):
+        if first == "1":
             g = Game(p0, p1)
-        elif(first == "2"):
+        elif first == "2":
             g = Game(p1,p0)
         else:
             break
         winner = g.run_all()
         print(winner)
-    elif (choice == "2"):
+    elif choice == "2":
         tn = telnetlib.Telnet('artemis.engr.uconn.edu', '4705')
         tn.read_until(b"?Username:")
         tn.write(b"42069\r\n")
@@ -33,20 +34,19 @@ if __name__ == '__main__':
         while True:
             res = tn.read_some()
             print(str(res, "utf-8"))
-            if ("Player:1" in str(res, "utf-8")):
+            if "Player:1" in str(res, "utf-8"):
                 p0 = NetworkPlayer(tn)
                 p1 = AIPlayer()
                 g = Game(p1, p0)
                 break
-            elif ("Player:2" in str(res, "utf-8")):
+            elif "Player:2" in str(res, "utf-8"):
                 p0 = NetworkPlayer(tn)
                 p1 = AIPlayer()
                 g = Game(p0, p1)
                 break
         winner = g.run_all()
         print(winner)
-
-    elif (choice == "3"):
+    elif choice == "3":
         p0 = AIPlayer()
         p1 = AIPlayer()
         g = Game(p0, p1)
