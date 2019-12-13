ece9b59bd56f3a0d0d4604aca28dbf7cfd84b654
Alex <alex.o'neill@uconn.edu>
2019-12-12 23:03:31 -0500

    Learning Started
    AI will only learn by playing itself or a human. The basic eval functions are written, some tweaking may be needed. Reward function still needs to be done. Since mulitple terms, that may need to change to have random coefficients and if it wins then they replace the current ones.

--- game/player.py
+++ game/player.py
@@ -1,6 +1,6 @@
 import pickle
 import math
-from .polynomial import (Polynomial, Term)
+from .polynomial import Polynomial
 import json
 
 cols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r']
@@ -58,11 +58,13 @@ def decode_from_server(arg):
 class AIPlayer(Player):
     def __init__(self, connection = None, learn = True):
         self.tn = connection
-        self._name = "Motley Crew"
         self._learn = learn
         self._polynomial = pickle.load(open('Memory/polynomial.data', 'rb'))
         if learn:
+            self._name = "Learner"
             self._path = []
+        else:
+            self._name = "Motley Crew"
 
     # Returns the best-worst-case (next move, score)
     def _minimax(self, curState, depth=4, alpha=-math.inf, beta=math.inf, maxPlayer=True):
@@ -97,22 +99,32 @@ class AIPlayer(Player):
         print(f'*** {self._name}\'s move ***')
         print(state)
 
+        # if self._learn:
+        #     bestMove = self._minimax(curState=state)[0]
+        #     print("Best move:")
+        #     print(bestMove)
+        # else:
+        #     moves = list(state.moves())
+        #     bestMove = moves[0]
+        #     print("Best move:")
+        #     print(bestMove)
         bestMove = self._minimax(curState=state)[0]
+        print("Best move:")
         print(bestMove)
 
         # If learning, save each new position and its static evaluation to self._path
         if self._learn:
-            if state.board().n_removed() <= 1: 
+            if state.board().n_removed() <= 1:
                 self._path.append((state, self._polynomial.evaluate(state)))
             newState = state + bestMove
             self._path.append((newState, self._polynomial.evaluate(newState)))
-        print(self._path)
-        
+            print(self._path)
+
         if self.tn is not None:
             self.tn.write(encode_for_server(str(bestMove)).encode('ascii') + b"\r\n")
         return bestMove
 
-    def getPath(self, state):
+    def getPath(self):
         return self._path
 
     def __str__(self):
